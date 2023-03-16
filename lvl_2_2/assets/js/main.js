f89b1d4761f79b78a6a29147f42d03378d092700
Rizvan555 <rizvan84@gmx.de>
2023-03-16 15:31:21 +0100

    changed splice index

--- lvl_2_2/assets/js/main.js
+++ lvl_2_2/assets/js/main.js
@@ -1,11 +1,11 @@
 const addLetters = () => {
   const input = document.querySelector("#inputLength").value;
   const result = document.querySelector("#result");
-  let loopArr = ["L", "o", "o", "p"];
+  let loopArr = ["L", "p"];
 
   const inputCount = Number(input);
   for (let index = 1; index <= inputCount; index++) {
-    loopArr.splice(2, 0, "o");
+    loopArr.splice(1, 0, "o");
   }
   const loopString = loopArr.join("");
   result.innerHTML = loopString;
